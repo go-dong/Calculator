b120d60347a261e83f23d0d8f5ac323655fcbcb5
ihb <gusqja4515@naver.com>
2018-10-19 12:11:47 +0900

    reset event
    리셋이 카운트 실행할때 설정했던 값으로 리셋되도록 수정

--- src/control.js
+++ src/control.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import * as Actions from './actions';
-import * as Defaults from './defaults';
+//import * as Defaults from './defaults';
 
 class Control extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      reset: false,
       curTime: '',
       resetTime: ''
     }
@@ -13,10 +14,16 @@ class Control extends Component {
     this.pauseCount = this.pauseCount.bind(this);
     this.resetCount = this.resetCount.bind(this);
   }
+  reset = false;
   startCount() {
+    //this.reset = false;
+    if(!this.state.reset) {
+      this.setState({
+        resetTime: this.props.store.getState().currentTime
+      });
+    }
     this.setState({
-      curTime: this.props.store.getState().currentTime,
-      resetTime: this.props.store.getState().currentTime
+      curTime: this.props.store.getState().currentTime
     });
     this.timer = setInterval(() => {
       this.setState({
@@ -29,20 +36,26 @@ class Control extends Component {
     }, 1000);
   }
   pauseCount() {
+    this.setState({
+      reset: true
+    });
     if(this.timer) {
       clearInterval(this.timer);
+      clearInterval(this.counter);
     }
     this.props.store.dispatch(Actions.resetTime(this.state.curTime));
   }
   resetCount() {
     if(this.timer) {
       clearInterval(this.timer);
+      clearInterval(this.counter);
       this.setState({
-        curTime: Defaults.defaultTime[1]
+        reset: false,
+        curTime: this.state.resetTime
       });
     }
     this.timer = null;
-    this.props.store.dispatch(Actions.resetTime(Defaults.defaultTime[1]));
+    this.props.store.dispatch(Actions.resetTime(this.state.resetTime));
   }
   render() {
     return (
